7a997073bda359c6701834eba2b5b9a8fe8e1639
John Nathaniel Calvara <nate@n8ventures.dev>
2026-07-26 03:40:43 +0800

    fix: rename windows exe after build

--- devtools.py
+++ devtools.py
@@ -216,7 +216,7 @@ def run_pyinstaller(build_label: str) -> int:
 
 def post_build_summary(build_label: str, count: int, success: bool):
     print("\n" + "─" * 60)
-    app_path = DIST_DIR / f"{APP.replace(" ", "").replace("'","") if win else APP}{EXT}"
+    app_path = DIST_DIR / f"{APP}{EXT}"
 
     if success:
         if app_path.is_dir():
@@ -641,6 +641,11 @@ def main():
 
     if returncode == 0:
         app_path = DIST_DIR / f"{APP}{EXT}"
+
+        if win:
+            renamed_app_path = DIST_DIR / f"{APP.replace(" ", "").replace("'","") if win else APP}{EXT}"
+            renamed_app_path.unlink(missing_ok=True)
+            app_path.rename(renamed_app_path)
 
         if mac:
             fix_ssl_dylib_conflict(app_path)
